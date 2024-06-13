a3a8bb84e5a844dfd4de289668e12b0b3608cc6f
vorteggs <sincereapologies@ya.ru>
2024-06-13 13:11:06 +0300

    port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { createServer } = require('node:http');
 const hostname = '0.0.0.0';
-const port = 3000;
+const port = 80;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
